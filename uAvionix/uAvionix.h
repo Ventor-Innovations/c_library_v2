9b122772578d65a785ee46001dcad4950e781f19
Rubén Pérez Alonso <rperez@ventorinnovations.es>
2025-12-02 13:55:17 +0100

    autogenerated headers for rev https://github.com/Ventor-Innovations/mavlink/tree/573ae28dd5738183831e6faaf608bcdbb5fdf275

--- uAvionix/uAvionix.h
+++ uAvionix/uAvionix.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_UAVIONIX.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_UAVIONIX_XML_HASH 6695163777933587175
+#define MAVLINK_UAVIONIX_XML_HASH -1211349369473762951
 
 #ifdef __cplusplus
 extern "C" {
